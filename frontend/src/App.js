fe369c69c52929c55577d6466e4a9b9471d00850
HovhannesTutyan <atutyanaz@gmail.com>
2022-11-26 01:22:37 +0400

    smooth scroll animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,63 @@
-import { useContext } from 'react';
-import { Provider } from 'react-redux';
-import {
-  BrowserRouter, 
-  Routes, 
-  Route 
-} from 'react-router-dom';
-import store from './store';
-import { DarkModeContext } from './context/darkModeContext';
-import { userInputs, productInputs } from './formSource';
-import Signup from './containers/auth/Signup';
-import Signin from './containers/auth/Login';
-import Home from "./pages/home/Home";
-import List from './pages/list/List';
-import Login from './pages/login/Login';
-import New from './pages/new/New';
-import Single from './pages/single/Single';
-import './style/dark.scss';
+import { useEffect, useState, useLayoutEffect } from "react";
+import Background1 from './ready-html/img/layer-base.png';
+import Background2 from './ready-html/img/layer-front.png';
+import Background3 from './ready-html/img/layer-middle.png';
+import Dungion from './ready-html/img/dungeon.jpg';
+import './ready-html/css/main.css';
+import { createContext } from "react";
+import { gsap } from "gsap-trial";
+import { ScrollTrigger } from "gsap-trial/dist/ScrollTrigger";
+import { ScrollSmoother } from "gsap-trial/dist//ScrollSmoother";
 
-function App() {
-
-  const {darkMode} = useContext(DarkModeContext);
-
+function App({ Component, pageProps }) {
+  const SmootherContext = createContext();
+  let [smoother, setSmoother] = useState();
+  const [scrollPosition, setScrollPosition] = useState(0);
+  useEffect(()=>{
+    const onScroll = (e) => {
+      setScrollPosition(e.target.documentElement.scrollTop);
+      
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  },[scrollPosition])
+  
+ useEffect(()=>{
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    smoother = ScrollSmoother.create({
+    wrapper:'#smooth-wrapper',
+    content:'#smooth-content',
+    smooth: 3,
+    effects: true,
+  });
+  smoother.effects("img", {speed:"auto"})
+ },[])
+ document.body.style.cssText=`--scrollTop: ${scrollPosition}px; height: 1570px;`
+ 
   return (
-    <Provider store={store}>
-      <div className={darkMode ? "app dark" : "app"}>
-        <BrowserRouter >
-          <Routes>
-            <Route exact path="/signup" element={<Signup/>} />
-            <Route exact path="/signin" element={<Signin/>} />
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/users" element={<List />} />
-            <Route path="/users/:userId" element={<Single />} />
-            <Route path="/users/new" element={<New inputs={userInputs} title="Add New User" />} />
-            <Route path="/products" element={<List />} />
-            <Route path="/products/:productId" element={<Single />} />
-            <Route path="/products/new" element={<New inputs={productInputs} title="Add New Product" />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </Provider>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <header className="main-header">
+            <div className="layers">
+              <div className="layer__header">
+                <div className="layers__caption">Welcome to Parallax</div>
+                <div className="layers__title">Fairy Forest</div>
+              </div>
+              <div className="layer layers__base" style={{backgroundImage: "url(" + Background1 + ")"}}></div>
+              <div className="layer layers__middle" style={{backgroundImage: "url(" + Background2 + ")"}}></div>
+              <div className="layer layers__front" style={{backgroundImage: "url(" + Background3 + ")"}}></div>
+            </div>
+          </header>
+          
+          <article className="main-article" style={{backgroundImage: "url(" + Dungion + ")"}}>
+            <div className="main-article__content">
+              <h2 className="main-article__header">To be continued</h2>
+              <p className="main-article__paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis error provident dignissimos facere. Repellendus tempore autem qui! Quia magnam tempora esse id necessitatibus corrupti mollitia expedita sapiente cum rerum, ut dicta laboriosam!</p>
+            </div>
+            <div className="copy">© WebDesign Master</div>
+          </article>
+        </div>
+	    </div>
     
   );
 }
